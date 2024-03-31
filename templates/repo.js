91aaa38dd8a1906286c9e661de1464bf600a6e18
Infatuation <5252107+infatuation5858@user.noreply.gitee.com>
2024-03-31 16:36:38 +0800

    commit: change template name

--- templates/repo.js
+++ templates/repo.js
@@ -2,8 +2,8 @@ import { process } from '../env/index.js';
 
 const repo = {
     'vue-webpack-template': {
-        url: 'https://github.com/Inatuation/vue2-webpack-template.git',
-        downloadUrl: process.env.NODE_ENV === 'development' ? '/Users/linyoujia/Desktop/学习/po-cli-template/vue-webpack' : 'https://github.com:Inatuation/vue2-webpack-template#main',
+        url: 'https://github.com/Inatuation/vue-webpack-template.git',
+        downloadUrl: process.env.NODE_ENV === 'development' ? '/Users/linyoujia/Desktop/学习/po-cli-template/vue-webpack' : 'https://github.com:Inatuation/vue-webpack-template#main',
         description: 'vue2 + webpack'
     }
 }
